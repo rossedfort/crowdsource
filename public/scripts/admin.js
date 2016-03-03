8922c44aceafe11f69fd825cc73f2088537ec361
rossedfort <rossedfort@gmail.com>
2016-03-03 11:02:15 -0700

    modify views and js files to use currentPoll to populate DOM

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -3,11 +3,9 @@ var socket = io();
 var aVoteCount = $("#a");
 var bVoteCount = $('#b');
 var cVoteCount = $('#c');
-var dVoteCount = $('#d');
 
 socket.on('voteCount', function (votes) {
   aVoteCount[0].innerHTML = votes.A;
   bVoteCount[0].innerHTML = votes.B;
   cVoteCount[0].innerHTML = votes.C;
-  dVoteCount[0].innerHTML = votes.D;
 });
