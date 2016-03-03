c814256745000f5c0c08b2741ad2d627bb348b68
rossedfort <rossedfort@gmail.com>
2016-03-03 13:56:30 -0700

    unique polls should work

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -4,7 +4,8 @@ var aVoteCount = $("#a");
 var bVoteCount = $('#b');
 var cVoteCount = $('#c');
 
-socket.on('voteCount', function (votes) {
+var hashChannel = localStorage.getItem('hash')
+socket.on(hashChannel, function (votes) {
   aVoteCount[0].innerHTML = votes.A;
   bVoteCount[0].innerHTML = votes.B;
   cVoteCount[0].innerHTML = votes.C;
--- server.js
+++ server.js
@@ -23,35 +23,35 @@ app.get('/new-poll', function (req, res){
 });
 
 app.post('/polls', function(req, res){
-  app.locals.currentPoll = {};
   var voterUrl = generator.generateVoterUrl(req);
   var adminUrl = generator.generateAdminUrl(req);
   var hash = generator.hash();
   var poll = pollBuilder.buildPoll(hash, req.body);
-  app.locals.currentPoll = poll
   var uniqueRef = new Firebase('https://burning-heat-1406.firebaseio.com/' + hash + '');
   uniqueRef.update({poll});
-  res.render(__dirname + '/public/views/polls.ejs', {voter: voterUrl, admin: adminUrl, hash: hash, poll: poll})
+  res.render(__dirname + '/public/views/polls.ejs', {voter: voterUrl, admin: adminUrl, poll: poll})
 });
 
 app.get('/voter/:id', function (req, res){
   var pathId = req.originalUrl.split('/')[2]
-  var currentPollId = app.locals.currentPoll.hash
-  if (pathId === currentPollId) {
-    res.render(__dirname + '/public/views/voter.ejs', {poll: app.locals.currentPoll})
-  } else {
+  var ref = new Firebase('https://burning-heat-1406.firebaseio.com/' + pathId + '');
+
+  ref.on("value", function(snapshot) {
+    res.render(__dirname + '/public/views/voter.ejs', {poll: snapshot.val()})
+  }, function (errorObject) {
     res.sendFile(__dirname + '/public/views/error.html')
-  }
+  });
 });
 
 app.get('/admin/:id', function (req, res){
   var pathId = req.originalUrl.split('/')[2]
-  var currentPollId = app.locals.currentPoll.hash
-  if (pathId === currentPollId) {
-    res.render(__dirname + '/public/views/admin.ejs', {poll: app.locals.currentPoll})
-  } else {
+  var ref = new Firebase('https://burning-heat-1406.firebaseio.com/' + pathId + '');
+
+  ref.on("value", function(snapshot) {
+    res.render(__dirname + '/public/views/admin.ejs', {poll: snapshot.val()})
+  }, function (errorObject) {
     res.sendFile(__dirname + '/public/views/error.html')
-  }
+  });
 });
 
 const port = process.env.PORT || 3000;
@@ -64,24 +64,18 @@ server.listen(port, function () {
 const io = socketIo(server);
 
 io.on('connection', function (socket) {
-  io.sockets.emit('userConnection', io.engine.clientsCount);
-
   socket.on('message', function (channel, message) {
-    if (channel === 'voteCast') {
-      votes[socket.id] = message;
-      socket.broadcast.emit('voteCount', countVotes(votes, app.locals.currentPoll));
-      socket.emit('voteCount', countVotes(votes));
-    }
+    votes[socket.id] = message;
+    socket.broadcast.emit(channel, countVotes(votes));
   });
 
   socket.on('disconnect', function () {
     delete votes[socket.id];
     socket.emit('voteCount', countVotes(votes));
-    io.sockets.emit('userConnection', io.engine.clientsCount);
   });
 });
 
-function countVotes(votes, currentPoll) {
+function countVotes(votes) {
   var voteCount = {
       A: 0,
       B: 0,
