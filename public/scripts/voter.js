e0758438e20ea145034adf617a454fbe1ebd2231
rossedfort <rossedfort@gmail.com>
2016-03-03 20:14:22 -0700

    finish sesison implementation

--- public/scripts/voter.js
+++ public/scripts/voter.js
@@ -7,20 +7,10 @@ var prompt = $('#prompt');
 localStorage.setItem('hash', location.pathname.split("/")[2])
 
 for (var i = 0; i < buttons.length; i++) {
-  if (sessionStorage.getItem('session') === null) {
-    buttons[i].addEventListener('click', function () {
-      sessionStorage.setItem('session', '87dcbwHLBS312o7bxads')
-      $.each(buttons, function(index, value) {value.remove()})
-      socket.send(localStorage.getItem('hash'), this.id);
-      yourVote.append(this.innerText)
-      prompt[0].innerHTML = "Thank you for your Vote!"
-    });
-  }
-  else {
-    yourVote.remove()
+  buttons[i].addEventListener('click', function () {
     $.each(buttons, function(index, value) {value.remove()})
-    prompt[0].innerHTML = "You've already cast your vote, you cheater"
-  }
+    socket.send(localStorage.getItem('hash'), this.id);
+    yourVote.append(this.innerText)
+    prompt[0].innerHTML = "Thank you for your Vote!"
+  });
 }
-
-console.log(sessionStorage);
