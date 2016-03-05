d4b2cf512c0e1163f346309cfd60caf85a666135
rossedfort <rossedfort@gmail.com>
2016-03-05 12:50:00 -0700

    tweak styling make minimum of 2 answers

--- public/scripts/poll.js
+++ public/scripts/poll.js
@@ -8,13 +8,13 @@ newAnswerButton.click(function(){
     alerts.append(`<p class=" alert red-text">You can only have 6 answers to a poll</p>`);
     $('.alert').fadeOut(2000);
   } else {
-    inputs.append(`<input class='answer' type="text" name="answer[]" placeholder="poll answer">`);
+    inputs.append(`<input class='answer' type="text" name="answer[]" placeholder="answer">`);
   }
 });
 
 removeAnswerButton.click(function(){
-  if ($('.answer').length === 1) {
-    alerts.append(`<p class=" alert red-text">You need to have at least 1 answer</p>`);
+  if ($('.answer').length === 2) {
+    alerts.append(`<p class=" alert red-text">You need to have at least 2 answers</p>`);
     $('.alert').fadeOut(2000);
   } else {
     $('.answer')[0].remove();
