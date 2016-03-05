19e037a7bbc3b201e1194083d76672866ee5b1ff
rossedfort <rossedfort@gmail.com>
2016-03-05 12:35:39 -0700

    finish add and remove answers functionality

--- public/scripts/poll.js
+++ public/scripts/poll.js
@@ -1,10 +1,22 @@
 var newAnswerButton = $('#newAnswer');
+var removeAnswerButton = $('#removeAnswer');
 var inputs = $('.inputs');
+var alerts = $('.alerts');
 
 newAnswerButton.click(function(){
   if ($('.answer').length === 6) {
-    alert('Only 6 answers Allowed')
+    alerts.append(`<p class=" alert red-text">You can only have 6 answers to a poll</p>`);
+    $('.alert').fadeOut(2000);
   } else {
     inputs.append(`<input class='answer' type="text" name="answer[]" placeholder="poll answer">`);
+  }
+});
+
+removeAnswerButton.click(function(){
+  if ($('.answer').length === 1) {
+    alerts.append(`<p class=" alert red-text">You need to have at least 1 answer</p>`);
+    $('.alert').fadeOut(2000);
+  } else {
+    $('.answer')[0].remove();
   }
 });
